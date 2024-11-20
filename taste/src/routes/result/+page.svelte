ffe148647926335bec72155b7da0f915c53feacb
Your Name <abdullaharifsalimee@gmail.com>
2024-11-20 20:45:52 +0500

    ok

--- taste/src/routes/result/+page.svelte
+++ taste/src/routes/result/+page.svelte
@@ -21,44 +21,59 @@
       </p>
     </div>
     <div
-      class="relative w-full h-fit grid grid-cols-12 grid-rows-3 py-10 my-20"
+      class="relative w-full h-fit grid grid-cols-12 grid-rows-4 py-5 my-20"
     >
       <div class="absolute inset-0 bg-[#080D15] opacity-70 z-0"></div>
-      <div class="row-span-3 col-span-2 z-10"></div>
-      <div class="row-span-3 col-span-2 z-10 flex justify-center items-center">
+      <div class="row-span-4 col-span-2 z-10 "></div>
+      <div class="row-span-4 col-span-2 z-10 flex justify-center items-center">
         <img src="../batman.jpg" alt="" class="rounded-2xl h-72 w-auto" />
       </div>
-      <div class="col-span-6 row-span-2 text-2xl m-2 z-10 font-poiret">
-        A manic killer known as "The Riddler" who is filled with rage and
-        determined to expose the corrupt system whilst picking off all of
-        Gotham's key political figures. Working with both established and new
-        allies, Bruce must track down the killer and see him brought to justice,
-        while investigating his father's true legacy and questioning the effect
-        that he has had on Gotham so far as "The Batman."
+      
+      
+      
+      <div class="col-span-6 row-span-1 justify-start items-center flex  text-5xl m-2 z-10 font-poiret">
+       The batman
       </div>
-      <div class="row-span-3 col-span-2"></div>
-      <button
-        class="z-10 row-span-1 col-span-2 bg-black opacity-70 m-2 rounded-xl font-poiret text-5xl text-white text-right flex justify-center items-center p-2"
-      >
-        <img src="../imdb.png" alt="" class="h-12 w-fit mx-2" />
-        <div class="mx-2">9.8</div>
+      <div class="row-span-4 col-span-2 "></div>
+     
+     <div class="col-span-6 row-span-2 text-xl m-2 z-10 font-poiret "> A manic killer known as "The Riddler" who is filled with rage and
+      determined to expose the corrupt system whilst picking off all of
+      Gotham's key political figures. Working with both established and new
+      allies, Bruce must track down the killer and see him brought to justice,
+      while investigating his father's true legacy and questioning the effect
+      that he has had on Gotham so far as "The Batman."</div>
+     
+      <button class="bg-[#231704] opacity-70  hover:opactiy-100 rounded-xl m-2 font-poiret flex justify-evenly items-center text-3xl p-2">
+
+        <img src="../imdb.png" alt="" class="h-8 ">
+        9.8
       </button>
-      <button
-        class="z-10 row-span-1 col-span-2 bg-white opacity-50 m-2 rounded-xl font-poiret text-5xl text-black"
-      >
-        Action
+     
+    
+      <button class="bg-[#231704] hover:opacity-100 opacity-70 rounded-xl m-2 font-poiret flex justify-evenly items-center text-3xl p-2">
+
+      Action
       </button>
-      <button
-        class="z-10 row-span-1 col-span-2 bg-custom-dark m-2 rounded-xl font-poiret text-5xl text-white"
-      >
-        More
-      </button>
+     
+    
+    
+    
     </div>
+ 
+ 
+ <button class="bg-[#FF0000] opacity-45 p-6 px-10 font-poiret text-4xl rounded-xl my-8 hover:opacity-100">
+
+Again
+
+
+ </button>
+ 
+ 
     <div
       class="flex w-fit h-fit bg-white p-4 rounded-xl justify-center items-center"
     >
       <button
-        class="bg-black rounded-full font-poiret w-16 h-16 text-2xl flex justify-center items-center mx-2"
+        class=" rounded-full bg-black font-poiret w-16 h-16 text-2xl flex justify-center items-center mx-2"
         >Join</button
       >
       <div class="text-black font-poiret text-2xl mx-2">
